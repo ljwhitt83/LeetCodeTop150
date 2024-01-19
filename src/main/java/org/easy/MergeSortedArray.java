ef140c033a8eb5832cf5c5021bd817e6acf6ef65
ljwhitt83 <ljwhitt83@gmail.com>
2024-01-19 14:40:58 -0700

    initial commit with one problem solved

---
+++ src/main/java/org/easy/MergeSortedArray.java
@@ -0,0 +1,30 @@
+package org.easy;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        //setting pointer one to the end of the valid elements in nums1
+        int pointer1 = m - 1;
+
+        //setting pointer two to the end of the valid elements in nums2
+        int pointer2 = n - 1;
+
+        //setting a pointer for the total of the merged array at nums1 completion
+        int pointerTotal = m+ n - 1;
+
+        //loop through the arrays comparing which one is larger and decreasing the count
+        while (pointer1 >= 0 && pointer2 >= 0){
+            if (nums1[pointer1] > nums2[pointer2]){
+                nums1[pointerTotal--] = nums1[pointer1--];
+            }else {
+                nums1[pointerTotal--] = nums2[pointer2--];
+            }
+        }
+
+        while(pointer2 >= 0) {
+            nums1[pointerTotal--] = nums2[pointer2--];
+        }
+
+    }
+}
+
